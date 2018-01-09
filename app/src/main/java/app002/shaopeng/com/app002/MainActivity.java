8d54afdc1cd2e1c5d75ce275404e32a940e630a8
shaop <shaop1005@foxmail.com>
2018-01-09 20:41:18 +0800

    Initial commit

---
+++ app/src/main/java/app002/shaopeng/com/app002/MainActivity.java
@@ -0,0 +1,13 @@
+package app002.shaopeng.com.app002;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
